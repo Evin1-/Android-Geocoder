2e9ecd0f3f1f8050274fcc3ae9a4646aa0ca1078
Evin1- <edwin.hdz.12.0@gmail.com>
2016-04-27 02:16:12 -0400

    Add Geocoder logic

--- app/src/main/java/com/example/simplegeocoder/MainActivity.java
+++ app/src/main/java/com/example/simplegeocoder/MainActivity.java
@@ -1,13 +1,35 @@
 package com.example.simplegeocoder;
 
+import android.location.Address;
+import android.location.Geocoder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivityTAG_";
+
+    private static final String SEARCH_ADDRESS = "3301 Windy Ridge Parkway";
+    private static final int MAX_RESULTS = 5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        try {
+            Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+            List<Address> addresses = geocoder.getFromLocationName(SEARCH_ADDRESS, MAX_RESULTS);
+            for (Address address : addresses) {
+                Log.d(TAG, "onCreate: " + address);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
